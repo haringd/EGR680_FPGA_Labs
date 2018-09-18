f6d6f61180e89dd163fbae0434d8e58fa643873e
haringd <haringd@mail.gvsu.edu>
2018-09-18 00:21:50 -0400

    Started with lab 3 including report, struggeling with the design that I have to make about how to implement the state machine.

---
+++ VENDMACH/VENDMACH.srcs/sources_1/new/vendmach_top.v
@@ -0,0 +1,16 @@
+module vendmach_top(
+    input SW0,
+    input SW1,
+    input BTN0,
+    input BTN1,
+    input BTN2,
+    input BTN3,
+    input clk,
+    input rst,
+    output [6:0] seg0,
+    output [6:0] seg1, // ? Pmod B
+    output [2:0] bar
+    );
+    
+
+endmodule
