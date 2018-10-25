4a139a88cd5cfa1511266046aff7e8d09200a80b
haringd <haringd@mail.gvsu.edu>
2018-10-25 18:20:16 -0400

    finished report lab 07 and ready for demo.

--- atm_class.py
+++ atm_class.py
@@ -5,159 +5,172 @@ Created on Thu Oct 18 18:34:49 2018
 @author: schwa
 """
 import re
-# Print Receipt
-def receipt(filename, file):
-    file.write('THANK YOU FOR CHOOSING BANK ME!')
-    file.close()
-    file = open(filename, "r")
-    for line in file:
-        print(line, end='')
-    file.close()
-    return 1
-# Balance 
-def balance( account_value , file):
-    print("\nYOUR BALANCE IS $%.2f \n \n\r" % account_value)
-    file.write('Your checking account balance is----> $'\
-               +str(account_value)+'\n\n')
-    delay(3000)
-    mainMenue( )
-    return 1
-# Withdraw money
-def withdraw( inVal, AMOUNT_MIN, AMOUNT_MAX, account_value, file ):
-    ret = [0, 0]; # no error 1, error 0
-    amount = 0
-    if not re.match("^[0-9]*$", inVal):  
+class Atm:
+    AMOUNT_MIN = 0
+    AMOUNT_MAX = 1000
+
+    Pin  = "1234"
+    Select = 0
+    AccountValue = 0
+    ReceiptFile = "atm_receipt.txt"
+    
+    def __init__(self, AccountValue, Pin):
+        self.AccountValue = AccountValue
+        self.Pin = Pin
+        
+    # Print Receipt
+    def receipt(self, filename, file):
+        file.write('THANK YOU FOR CHOOSING BANK ME!')
+        file.close()
+        file = open(filename, "r")
+        for line in file:
+            print(line, end='')
+        file.close()
+        return 1
+    # Balance 
+    def balance(self, account_value , file):
+        print("\nYOUR BALANCE IS $%.2f \n \n\r" % account_value)
+        file.write('Your checking account balance is----> $'\
+                   +str(account_value)+'\n\n')
+        self.delay(3000)
+        self.mainMenue( )
+        return 1
+    # Withdraw money
+    def withdraw(self,  inVal, AMOUNT_MIN, AMOUNT_MAX, account_value, file ):
+        ret = [0, 0]; # no error 1, error 0
         amount = 0
-        print("\nInvalid Input only nmbers are alowed.\n")
-        ret=[0, inVal]
-    else:    
-        amount = int(inVal)
-        if amount >= AMOUNT_MIN and amount <= account_value :  
-            account_value = account_value - amount
-            print("\nWITHDRAW AMOUNT $%.2f\n" % amount)   
-            file.write('Your withdraw amounte is------------> $'\
-                       +str(amount)+'\n\n')
-            amount = 0  
-            delay(3000)
-            mainMenue( )
-            ret=[1, account_value]
-        else:
+        if not re.match("^[0-9]*$", inVal):  
+            amount = 0
+            print("\nInvalid Input only nmbers are alowed.\n")
             ret=[0, inVal]
-            print("\nWITHDRAW AMOUNT $%.2f TOO BIG OR TOO SMALL\n" % amount) 
-    return ret
-# Deposit money
-def deposit( inVal, AMOUNT_MIN, AMOUNT_MAX, account_value, file ):
-    ret = [0, 0]; # no error 1, error 0
-    amount = 0
-    if not re.match("^[0-9]*$", inVal):  
+        else:    
+            amount = int(inVal)
+            if amount >= AMOUNT_MIN and amount <= account_value :  
+                account_value = account_value - amount
+                print("\nWITHDRAW AMOUNT $%.2f\n" % amount)   
+                file.write('Your withdraw amounte is------------> $'\
+                           +str(amount)+'\n\n')
+                amount = 0  
+                self.delay(3000)
+                self.mainMenue( )
+                ret=[1, account_value]
+            else:
+                ret=[0, inVal]
+                print("\nWITHDRAW AMOUNT $%.2f TOO BIG OR TOO SMALL\n" % amount) 
+        return ret
+    # Deposit money
+    def deposit( self, inVal, AMOUNT_MIN, AMOUNT_MAX, account_value, file ):
+        ret = [0, 0]; # no error 1, error 0
         amount = 0
-        print("\nInvalid Input only numbers are alowed.\n")
-        ret=[0, inVal]
-    else:    
-        amount = int(inVal)
-        if amount >= AMOUNT_MIN and amount <= AMOUNT_MAX :  
-            account_value = account_value + amount
-            print("\nDEPOSIT AMOUNT $%.2f\n" % amount)   
-            file.write('Your deposit amounte is-------------> $'\
-                       +str(amount)+'\n\n')
-            amount = 0  
-            delay(3000)
-            mainMenue( )
-            ret=[1, account_value]
-        else:
+        if not re.match("^[0-9]*$", inVal):  
+            amount = 0
+            print("\nInvalid Input only numbers are alowed.\n")
             ret=[0, inVal]
-            print("\nDEPOSIT AMOUNT $%.2f TOO BIG OR TOO SMALL\n" % amount) 
-    return ret
-# set new pin
-def newPin( inVal, file ):
-    ret = [0, 0]; # no error 1, error 0
-    if not re.match("^[0-9]{4}$", inVal):
-        print ("Error! Make sure you only use numbers from 0-9 in PIN")
-        delay(3000)
-        mainMenue( )
-        inVal = 'Z'      
-        ret=[0, inVal]
-    else:
-        ret =[1, inVal]  
-#        print(ret) # debug only
-        print("\nYOUR NEW PIN IS", inVal, "\n")   
-        file.write('You have changed your PIN number\n\n')
-        delay(3000)
-        mainMenue()
-    return ret
-# selection main menue
-def selMainMenue( inVal ):
-    ret = 0; # no error 1, error 0
-    if not re.match("^[1-5]*$", inVal):
-        print ("Error! Make sure you only use numbers from 1-5 in selecion")
-        ret=[0, inVal]
-    else:
-        ret =[1, inVal]
-    return ret
-# PIN validation
-def pin( inVal, PIN ):
-    ret = 0; # no error 1, error 0
-#    while True:
-    if not re.match("^[0-9]{4}$", inVal):
-        print ("Error! Make sure you only use numbers from 0-9 in PIN")
-        inVal = 'Z'    
-        ret = 0
-    else:
-        if inVal == PIN :    
-            #print("\nCorect PIN") # debug only
-            print("\n")
-            mainMenue()
-            inVal = 'Z'  
-            ret = 1
-#                break             
-        else:        
-            print("\nInvalid PIN!")
-            inVal = 'Z'  
+        else:    
+            amount = int(inVal)
+            if amount >= AMOUNT_MIN and amount <= AMOUNT_MAX :  
+                account_value = account_value + amount
+                print("\nDEPOSIT AMOUNT $%.2f\n" % amount)   
+                file.write('Your deposit amounte is-------------> $'\
+                           +str(amount)+'\n\n')
+                amount = 0  
+                self.delay(3000)
+                self.mainMenue( )
+                ret=[1, account_value]
+            else:
+                ret=[0, inVal]
+                print("\nDEPOSIT AMOUNT $%.2f TOO BIG OR TOO SMALL\n" % amount) 
+        return ret
+    # set new pin
+    def newPin( self, inVal, file ):
+        ret = [0, 0]; # no error 1, error 0
+        if not re.match("^[0-9]{4}$", inVal):
+            print ("Error! Make sure you only use numbers from 0-9 in PIN")
+            self.delay(3000)
+            self.mainMenue( )
+            inVal = 'Z'      
+            ret=[0, inVal]
+        else:
+            ret =[1, inVal]  
+    #        print(ret) # debug only
+            print("\nYOUR NEW PIN IS", inVal, "\n")   
+            file.write('You have changed your PIN number\n\n')
+            self.delay(3000)
+            self.mainMenue()
+        return ret
+    # selection main menue
+    def selMainMenue(self, inVal ):
+        ret = 0; # no error 1, error 0
+        if not re.match("^[1-5]*$", inVal):
+            print ("Error! Make sure you only use numbers from 1-5 in selecion")
+            ret=[0, inVal]
+        else:
+            ret =[1, inVal]
+        return ret
+    # PIN validation
+    def pin( self, inVal, PIN ):
+        ret = 0; # no error 1, error 0
+    #    while True:
+        if not re.match("^[0-9]{4}$", inVal):
+            print ("Error! Make sure you only use numbers from 0-9 in PIN")
+            inVal = 'Z'    
             ret = 0
-    return ret
-
-def delay( msec ):
-    cnt = 0
-    while cnt < msec:
-        cnt += 0.0001
-    return 1  # no error 1, error 0
-
-def mainMenue( ):
-    print(" Main Menu")
-    print("----------------------- \n")
-    print(" 1. CHECK BALANCE")
-    print(" 2. WITHDRAW CASH")
-    print(" 3. DEPOSIT CASH")
-    print(" 4. CHANGE PIN")
-    print(" 5. EXIT ")
-    return 1  # no error 1, error 0
-
-def welcome( ):
-    print("\n    $$$$$          $$          $$$          $$   $$     $  ")
-    print("    $    $        $$$$         $$ $         $$   $$    $   ")
-    print("    $     $       $   $        $$  $        $$   $$   $    ") 
-    print("    $     $      $$   $$       $$   $       $$   $$  $     ")  
-    print("    $    $      $$     $$      $$    $      $$   $$ $      ")  
-    print("    $$$$$       $$     $$      $$     $     $$   $$$       ")  
-    print("    $$$$$       $$$$$$$$$      $$      $    $$   $$$       ")  
-    print("    $    $     $$       $$     $$       $   $$   $$ $      ")    
-    print("    $     $   $$         $$    $$        $  $$   $$  $     ")   
-    print("    $     $   $$         $$    $$         $ $$   $$   $    ")   
-    print("    $    $   $$           $$   $$          $$$   $$    $   ")  
-    print("    $$$$$    $$           $$   $$           $$   $$     $  \n") 
-    print("             $$$          $$   $$$$$$$$$$$$$$$  ")
-    print("             $$ $        $$$   $$$$$$$$$$$$$$$   ")
-    print("             $$  $      $ $$   $$      ") 
-    print("             $$   $    $  $$   $$     ")  
-    print("             $$    $  $   $$   $$     ")  
-    print("             $$     $$    $$   $$$$$$$$$$$$$$$       ")  
-    print("             $$           $$   $$$$$$$$$$$$$$$       ")  
-    print("             $$           $$   $$      ")    
-    print("             $$           $$   $$      ")   
-    print("             $$           $$   $$      ")   
-    print("             $$           $$   $$$$$$$$$$$$$$$   ")  
-    print("             $$           $$   $$$$$$$$$$$$$$$  ")  
-    print("------------------------------------------------------------")
-    print("                    Welcome to Bank Me  ")
-    print("============================================================")
-    return 1  # no error 1, error 0
+        else:
+            if inVal == PIN :    
+                #print("\nCorect PIN") # debug only
+                print("\n")
+                self.mainMenue()
+                inVal = 'Z'  
+                ret = 1
+    #                break             
+            else:        
+                print("\nInvalid PIN!")
+                inVal = 'Z'  
+                ret = 0
+        return ret
+    
+    def delay(self, msec ):
+        cnt = 0
+        while cnt < msec:
+            cnt += 0.0001
+        return 1  # no error 1, error 0
+    
+    def mainMenue(self):
+        print(" Main Menu")
+        print("----------------------- \n")
+        print(" 1. CHECK BALANCE")
+        print(" 2. WITHDRAW CASH")
+        print(" 3. DEPOSIT CASH")
+        print(" 4. CHANGE PIN")
+        print(" 5. EXIT ")
+        return 1  # no error 1, error 0
+    
+    def welcome(self):
+        print("\n    $$$$$          $$          $$$          $$   $$     $  ")
+        print("    $    $        $$$$         $$ $         $$   $$    $   ")
+        print("    $     $       $   $        $$  $        $$   $$   $    ") 
+        print("    $     $      $$   $$       $$   $       $$   $$  $     ")  
+        print("    $    $      $$     $$      $$    $      $$   $$ $      ")  
+        print("    $$$$$       $$     $$      $$     $     $$   $$$       ")  
+        print("    $$$$$       $$$$$$$$$      $$      $    $$   $$$       ")  
+        print("    $    $     $$       $$     $$       $   $$   $$ $      ")    
+        print("    $     $   $$         $$    $$        $  $$   $$  $     ")   
+        print("    $     $   $$         $$    $$         $ $$   $$   $    ")   
+        print("    $    $   $$           $$   $$          $$$   $$    $   ")  
+        print("    $$$$$    $$           $$   $$           $$   $$     $  \n") 
+        print("             $$$          $$   $$$$$$$$$$$$$$$  ")
+        print("             $$ $        $$$   $$$$$$$$$$$$$$$   ")
+        print("             $$  $      $ $$   $$      ") 
+        print("             $$   $    $  $$   $$     ")  
+        print("             $$    $  $   $$   $$     ")  
+        print("             $$     $$    $$   $$$$$$$$$$$$$$$       ")  
+        print("             $$           $$   $$$$$$$$$$$$$$$       ")  
+        print("             $$           $$   $$      ")    
+        print("             $$           $$   $$      ")   
+        print("             $$           $$   $$      ")   
+        print("             $$           $$   $$$$$$$$$$$$$$$   ")  
+        print("             $$           $$   $$$$$$$$$$$$$$$  ")  
+        print("------------------------------------------------------------")
+        print("                    Welcome to Bank Me  ")
+        print("============================================================")
+        return 1  # no error 1, error 0
--- atm_instance.py
+++ atm_instance.py
@@ -6,30 +6,25 @@ This is a temporary script file.
 """
 from time import asctime
 import re
-from atm_func_partII import welcome, pin, \
-selMainMenue, newPin, deposit, withdraw, balance, receipt, delay
+#from atm_class import welcome, pin, \
+#selMainMenue, newPin, deposit, withdraw, balance, receipt, delay
+from atm_class import Atm
 
-AMOUNT_MIN = 0
-AMOUNT_MAX = 1000
-
-gPin  = "1234"
-gSelect = 0
-gAccountValue = 0
-gReceiptFile = "atm_receipt.txt"
+atm = Atm(100,'2345') # instantiate class Atm to atm object
 
 # Open a file
 try:
     fo = open("07_lab_error_log.txt", "a")
-    fr = open(gReceiptFile, "w")
-    fo.write(asctime()+ 'ATM program starts '+ '\n')
+    fr = open(atm.ReceiptFile, "w")
+    fo.write(asctime()+ ' ATM program starts '+ '\n')
     fr.write('\n-------------------Bank Me-------------------\n')
     fr.write('---------------Transaction Receipt-----------\n')
     fr.write('*********************************************\n\n')
 except IOError as e:
-    print('File '+e.filename+' could not be opened!') 
+    print('File '+e.filename+' could not be opened or writen to!') 
 
 # Star program
-if welcome(): 
+if atm.welcome(): # first method used from the atm object
     error = 0 
 else:
     error = 1
@@ -38,7 +33,7 @@ else:
 # PIN validation
 while True:
     ret = 0
-    if pin( input("Please enter your PIN: "), gPin): 
+    if atm.pin( input("Please enter your PIN: "), atm.Pin): 
         error = 0 
         fo.write(asctime()+ ' User logged in \n')
         break   
@@ -47,83 +42,83 @@ while True:
         fo.write(asctime()+ ' User login error\n')
 
 # Main menu
-while  gSelect != "5":
+while  atm.Select != "5":
     ret = 0
-    ret = selMainMenue(input("PLEASE SELECT FROM THE ABOVE OTPIONS: ") )
+    ret = atm.selMainMenue(input("PLEASE SELECT FROM THE ABOVE OTPIONS: ") )
 #    print(ret) # debug only
     if (ret[0]):
         error = 0 
-        gSelect = ret[1] 
+        atm.Select = ret[1] 
 #        print(ret[1] ) # debug only
     else: # error
         error = 1
-        gSelect = 0
+        atm.Select = 0
         fo.write(asctime()+ ' User slection error\n')
         print(ret[0])
 
-    if gSelect == '5': # exit
+    if atm.Select == '5': # exit
         while True:
             if not re.match("^[y]$", input('Do you wish to print the receipt, press y or n? ')):
                 print("\nThank you for chosing BANK ME!")
                 break
             else:
-                receipt(gReceiptFile, fr)   
+                atm.receipt(atm.ReceiptFile, fr)   
                 break
         exit
-    elif gSelect == '4': # change PIN
+    elif atm.Select == '4': # change PIN
         while True:
             ret = 0
-            ret = newPin(input("ENTER YOUR NEW PIN: "), fr)
+            ret = atm.newPin(input("ENTER YOUR NEW PIN: "), fr)
 #            print(ret) # debug only
             if (ret[0]):
                 error = 0 
-                gPin = ret[1] 
+                atm.Pin = ret[1] 
         #        print(ret[1] ) # debug only
                 break
             else: # error
                 error = 1
-                gSelect = 0
+                atm.Select = 0
                 fo.write(asctime()+ ' User PIN error\n')
                 break
 
-    elif gSelect == '3': # Deposit
+    elif atm.Select == '3': # Deposit
         while True:
             ret = 0
-            ret = deposit(input("ENTER YOUR DEPOSIT AMOUNT IN $: ")\
-                          , AMOUNT_MIN, AMOUNT_MAX, gAccountValue, fr)
+            ret = atm.deposit(input("ENTER YOUR DEPOSIT AMOUNT IN $: ")\
+                          , atm.AMOUNT_MIN, atm.AMOUNT_MAX, atm.AccountValue, fr)
 #            print(ret) # debug only
             if (ret[0]):
                 error = 0 
-                gAccountValue = ret[1] 
+                atm.AccountValue = ret[1] 
         #        print(ret[1] ) # debug only
                 break
             else: # error
                 error = 1
-                gSelect = 0
+                atm.Select = 0
                 fo.write(asctime()+ ' Deposit error\n')
  
-    elif gSelect == '2': # Withdraw
+    elif atm.Select == '2': # Withdraw
         while True:
             ret = 0
-            ret = withdraw(input("ENTER YOUR WITHDRAW AMOUNT IN $: ")\
-                           , AMOUNT_MIN, AMOUNT_MAX, gAccountValue, fr)
+            ret = atm.withdraw(input("ENTER YOUR WITHDRAW AMOUNT IN $: ")\
+                           , atm.AMOUNT_MIN, atm.AMOUNT_MAX, atm.AccountValue, fr)
 #            print(ret) # debug only
             if (ret[0]):
                 error = 0 
-                gAccountValue = ret[1] 
+                atm.AccountValue = ret[1] 
         #        print(ret[1] ) # debug only
                 break
             else: # error
                 error = 1
-                gSelect = 0
+                atm.Select = 0
                 fo.write(asctime()+ ' Withdraw error\n')            
          
-    elif gSelect == '1': # Balance
-        balance(gAccountValue, fr)
+    elif atm.Select == '1': # Balance
+        atm.balance(atm.AccountValue, fr)
     else:
         print("\nIncorrect Selecion!")
 
 fo.write(asctime()+ ' Program Closed\n')
 fo.close() # to ensure file is closed
 fr.close() # to ensure file is closed
-delay(3000)
+atm.delay(3000)
